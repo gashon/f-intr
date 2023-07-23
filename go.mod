71d903ceb32cb7121a403797c4db86365569fbd5
GashonHussein <gashon96@gmail.com>
2023-07-23 15:25:01 -0700

    chore: init token + lexer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gashon/f-intr
+
+go 1.18
---
+++ lexer/lexer_test.go
@@ -0,0 +1,42 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/gashon/f-intr/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := NewLexer(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("test[%d] - tokentype wrong. Expected %q, got %q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("test[%d] - literal wrong. Expected %q, got %q", i, tt.expectedLiteral, tok.Literal)
+		}
+
+	}
+
+}
